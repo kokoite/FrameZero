372b4fe64bcd3820ad929d1ad22f7861f4a9b4d1
Pranjal <kokoite908@gmail.com>
2026-04-25 11:43:30 +0530

    Initial Commit

---
+++ AnimationEngine/AnimationEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationEngineApp.swift
+//  AnimationEngine
+//
+//  Created by pranjal.agarwal on 25/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationEngine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimationEngine
+//
+//  Created by pranjal.agarwal on 25/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
